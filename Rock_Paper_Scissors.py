840b5d2e42ccf02c04a1651848dae63e63d317f6
akrztrk <akarozturk1@gmail.com>
2022-12-20 12:10:31 +0100

    Python

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,29 +1,28 @@
 import random
 
-while True:
-    user_action = input("What do you choose? Type 0 for rock, 1 for Paper or 2 for Scissors: ")
-    # I wanted the user to choose a number between 0 and 2 and converted it to an integer.
-    possible_actions = ["rock", "paper", "scissors"]
-    computer_action = random.choice(possible_actions)
-    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+print("Winning Rules of the Rock paper scissor game as follows: \n"
+      "Rock vs paper->paper wins \n"
+      "Rock vs scissor->Rock wins \n"
+      "paper vs scissor->scissor wins")
 
-    if user_action == computer_action:
-        print(f"Both players selected {user_action}. It's a tie!")
-    elif user_action == "rock":
-        if computer_action == "scissors":
-            print("Rock smashes scissors! You win!")
-        else:
-            print("Paper covers rock! You lose.")
-    elif user_action == "paper":
-        if computer_action == "rock":
-            print("Paper covers rock! You win!")
-        else:
-            print("Scissors cuts paper! You lose.")
-    elif user_action == "scissors":
-        if computer_action == "paper":
-            print("Scissors cuts paper! You win!")
-        else:
-            print("Rock smashes scissors! You lose.")
+while True:
+    user_choice = int(input("What do you choose? Type 0 for rock, 1 for Paper or 2 for Scissors: \n"))
+    # I wanted the user to choose a number between 0 and 2 and converted it to an integer.
+    computer_choice = random.randint(0, 2)
+    print(f"\nYou chose {user_choice}, computer chose {computer_choice}.\n")
+
+    if user_choice >= 3 or user_choice < 0:
+        print("You typed an invalid number, you lose")
+    elif computer_choice == 2 and user_choice == 0:
+        print("You wins")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif computer_choice < user_choice:
+        print("You wins")
+    elif computer_choice == user_choice:
+        print("It's draw")
 
     play_again = input("Play again? (y/n): ")
     if play_again.lower() != "y":
