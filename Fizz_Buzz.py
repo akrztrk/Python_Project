b54faaafac469f181c474ac55e5563e70eb923d1
akrztrk <akarozturk1@gmail.com>
2022-12-23 21:50:21 +0100

    Python

---
+++ Fizz_Buzz.py
@@ -0,0 +1,12 @@
+
+number = 0
+for i in range(1, 101):
+    number += 1
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
